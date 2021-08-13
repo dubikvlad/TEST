b1af7383388f990ee6adcc4df4371875c2884b90
Vlad Dubovski <dubikvlad@mail.ru>
2021-08-13 12:38:40 +0300

    added nav js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const navItems = document.querySelectorAll('.nav__item');
+
+for (let i = 0; i < navItems.length; i++) {
+    const el = navItems[i];
+    el.addEventListener('click', () => {
+        removeActive();
+        addActive(el);
+    });
+}
+
+function addActive(item) {
+    item.classList.add('active');
+}
+
+function removeActive() {
+    for (let i = 0; i < navItems.length; i++) {
+        const el = navItems[i];
+        el.classList.remove('active');
+    }    
+}
